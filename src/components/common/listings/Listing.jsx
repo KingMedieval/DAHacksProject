6e9baf1b27e2e18144ab1b9d809f9128cbdf97be
tomo <tshimada@purdue.edu>
2022-10-08 06:14:11 -0700

    Changes by Metamasker

--- src/components/common/listings/Listing.jsx
+++ src/components/common/listings/Listing.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
+import './Post.css'
+
 function Listing() {
-    return(
-        <div>
-            
-        </div>
-    );
-};
+    return (
+        <section className = "listing.outer">
+            <section className = "listing-text">
+                <p>Social Media Website</p>
+            </section>
+            <section className = "join-bottom">
+                <button className="join-button"> </button>
+            </section>
+        </section>
+    )
+}
 
 export default Listing;
