28412a8105830b080ff72b2a1ba118bc8aed903c
tomo <tshimada@purdue.edu>
2022-10-08 06:14:03 -0700

    Changes by ctrsh

---
+++ src/components/common/submit/Submit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Submit.css'
+
+function Submit() {
+    return (
+        <button className={"button-submit"}>SUBMIT</button>
+    )
+
+}
+
+export default Submit;
