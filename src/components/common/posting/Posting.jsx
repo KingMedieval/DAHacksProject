1dfbed0bed060a7326b35e76a568ebb8727e7982
KingMedieval <tshimada@purdue.edu>
2022-10-08 19:58:08 -0700

    Changes by ctrsh

--- src/components/common/posting/Posting.jsx
+++ src/components/common/posting/Posting.jsx
@@ -20,8 +20,9 @@ function Posting(props) {
                         <section className="listing-info">
                             <p className="listing-title"> {props.name}</p>
                             <p className="listing-desc">{props.desc}</p>
-                            <p className="numMembers">{props.numMembers}</p>
+                            <p className="numMembers">1/{props.numMembers} members</p>
                         </section>
+
                         <section className="listing-extra">
                             <img
                                 src="https://cdn.discordapp.com/attachments/1027411637591875624/1028324317123072060/profile-placeholder.png"
@@ -35,11 +36,8 @@ function Posting(props) {
                             <button className="listing-join" onClick={handleShow}>JOIN</button>
                             <button className="listing-learn" onClick={handleShow}>CONTACT</button>
                         </section>
-                        <section className="listing-tags">
-                            <a className="listing-tag1">{props.tag1}</a>
-                            <a className="listing-tag2">{props.tag2}</a>
-                        </section>
                     </section>
+
             </>
         );
 }
